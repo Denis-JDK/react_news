4930ebf2e082578b15138f6e9e4d137384b098ec
Shapoval Denis <speleo04@yandex.ru>
2023-09-21 22:34:32 +0300

    Added pages

--- src/components/NewsItem/NewsItem.js
+++ src/components/NewsItem/NewsItem.js
@@ -1,7 +1,10 @@
 
 import style from './NewsItem.module.css'
-import {unixToDate} from "../utils/utils";
+import {unixToDate} from "../../utils/utils";
+import {Link} from "react-router-dom";
+
 export function NewsItem(props){
+    const {className = '', title, username, date, score} = props
     const scoreClassArr = [style.score]
 
     if (props.score > 50){
@@ -12,17 +15,18 @@ export function NewsItem(props){
 
 
     return(
-        <div className={style.container}>
-            <a className={style.link} href="example.com">{props.title}</a>
+        <div className={`${style.container} ${className}`}>
+            <Link className={style.link} to={`comments/${props.id}`}>{title}</Link>
+            {/*<a className={style.link} href="example.com">{title}</a>*/}
 
             <div className={style.info}>
                 <div className={style.userDate}>
-                    <span>{props.username}</span>
-                    <span>{unixToDate(props.date)}</span>
+                    <span>{username}</span>
+                    <span>{unixToDate(date)}</span>
                 </div>
 
                 <div className={scoreClassArr.join(' ')}>
-                    {props.score} points
+                    {score} points
                 </div>
             </div>
         </div>
